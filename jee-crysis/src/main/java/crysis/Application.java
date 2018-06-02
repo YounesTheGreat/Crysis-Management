485bc590d2dc72de5f0585f559b97932ec22c981
younes <youneskasri@gmail.com>
2018-06-02 16:55:45 +0000

    Adds project structuration

---
+++ jee-crysis/src/main/java/crysis/Application.java
@@ -0,0 +1,13 @@
+package crysis;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ jee-crysis/src/main/java/crysis/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package crysis;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	@Autowired
+	private DataSource datasource;
+	
+	
+}
